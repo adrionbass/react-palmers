03968bba586370297f34bbad0e35295ffeb2cb98
Adriel Miño <adrielsmb@gmail.com>
2023-03-07 13:30:45 -0300

    7/3/2023h

--- src/components/GenericGrid.jsx
+++ src/components/GenericGrid.jsx
@@ -5,48 +5,37 @@ export default (props) => {
 };
 
 const GenericGrid = styled.div`
- ${'' /*  padding: 0 5rem; */}
   display: grid;
   gap: 1rem;
   grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
   grid-auto-rows: auto;
-   
+
   img {
     width: 100%;
     height: auto;
     object-fit: cover;
     border-radius: 7px;
 
-    
-
-
     @media screen and (max-width: 620px) {
       grid-template-columns: repeat(auto-fit, minmax(5rem, 1fr));
-
     }
-
-
-
-
   }
   .nos-txt {
     text-align: center;
     @media screen and (min-width: 620px) {
       margin: auto;
       text-align: right;
-
     }
 
     @media screen and (min-width: 840px) {
-
       margin-top: 10%;
     }
     @media screen and (min-width: 940px) {
-      ${'' /* grid-template-columns: repeat(3, 1fr); */}
-      
+      ${"" /* grid-template-columns: repeat(3, 1fr); */}
+
       &:nth-child(1) {
-      grid-row: 1 / span 2;
-    }
+        grid-row: 1 / span 2;
+      }
     }
   }
 `;
--- src/components/Scaffold.jsx
+++ src/components/Scaffold.jsx
@@ -1,23 +1,26 @@
 import styled from "styled-components";
 
 export default (props) => {
-  return <Section id={props.id}>
-  {props.children}
-  </Section>;
+  return <Section id={props.id}>{props.children}</Section>;
 };
 const Section = styled.section`
   color: whitesmoke;
   text-align: center;
-  ${'' /* padding: 30px 10%; */}
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
   h2 {
+    font-size: 3rem;
     margin-bottom: 20px;
     letter-spacing: 0.5rem;
-    
+    transition: 0.3s all;
+    &:hover {
+      letter-spacing: 0.8rem;
+      text-decoration-line: underline;
+      text-decoration-color: #f16767;
+    }
   }
 
   p {
@@ -30,7 +33,6 @@ const Section = styled.section`
     margin-top: 20px;
   }
 
-  
   .date {
     font-size: 0.8rem;
     font-style: italic;
@@ -38,27 +40,23 @@ const Section = styled.section`
     margin: 0;
   }
 
-  svg{
+  svg {
     margin-top: 10px;
   }
 
-  @media screen and (max-width: 760px) {
-
+  @media screen and (max-width: 480px) {
+    h2 {
+      &:hover {
+        letter-spacing: 0;
+      }
+    }
   }
 
   @media (max-width: 320px) {
     h2 {
       text-align: center;
       letter-spacing: 0.2rem;
+
     }
   }
 `;
-
-const Line = styled.div`
-  width: 150px;
-  height: 4px;
-  background: #fc036b;
-  margin: 10px auto;
-  border-radius: 5px;
-
-`
